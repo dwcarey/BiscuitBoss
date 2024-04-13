c067f54ed2d9a822ce4a7b94b03eb17a806cf841
daniel <danielwilliamcarey@gmail.com>
2024-04-13 20:00:31 +0800

    adjusted vite config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/BiscuitBoss/',  // Adjust this to match your repository path
     plugins: [react()],
     build: {
-        outDir: 'build'  // Specify the output directory
+        outDir: 'build'
     }
 });
